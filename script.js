0bcc0eda217cc80229d3a1c207a3ca58e5ba02a5
jadielalves <jadielweb@gmail.com>
2026-09-09 11:41:54 -0300

    Função de adicionar icone check e pequena refatoração

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const taskDialog = document.getElementById('task-dialog')
 
 const tasks = []
 
+function addCheckIcon(button){
+    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>'
+}
+
 function createTask(task){
     const splitHour = task.hour.split(':')
 
@@ -21,16 +25,16 @@ function createTask(task){
 
     const checkButton = document.createElement('button')
     checkButton.classList.add('check-button')
-    if(task.completed === true){
+    if(task.completed){
         checkButton.classList.add('completed')
-        checkButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>'
+        addCheckIcon(checkButton)
     }
     
     
     const newTask = document.createElement("li")
     const newTaskText = document.createElement('span')
     newTaskText.textContent = task.name
-    if(task.completed === true){
+    if(task.completed){
         newTaskText.classList.add('completed-task')
     }
 
@@ -43,7 +47,8 @@ function createTask(task){
     }
     else{
         const hourPM = hour - 12
-        newTaskHour.textContent = `0${hourPM}:${minutes} PM`
+        const formattedHour = String(hourPM).padStart(2, '0')
+        newTaskHour.textContent = `${formattedHour}:${minutes} PM`
     }
 
     const deleteButton = document.createElement('button')
